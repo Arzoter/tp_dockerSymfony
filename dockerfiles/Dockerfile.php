01ac54960f1d7cdcee5d5d734032ca96b241882a
Arzoter <arthur.cloux@gmail.com>
2023-11-01 14:14:00 +0100

    build: added Dockerfiles & compose for mendatory services

--- dockerfiles/Dockerfile.php
+++ dockerfiles/Dockerfile.php
@@ -1,3 +1,10 @@
-FROM php:8.1
-# RUN symfony serve
-# CMD bin/sh
+FROM php:8.1-fpm AS dev
+
+# Installer les dépendances nécessaires pour le développement
+RUN apt-get update && apt-get install -y \
+    unzip \
+    git \
+    libzip-dev
+
+# Installer les extensions PHP nécessaires
+RUN docker-php-ext-install pdo pdo_mysql
